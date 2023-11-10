6b9ff26353560ffbc3bef94404fe3578404113b6
Michaiah Bos <michaiahbos@yahoo.com>
2023-11-10 13:36:25 -0600

    working on it

--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Todo from './Todo'
 
 export default class TodoList extends React.Component {
+
   render() {
+    const {todos, toggleCompletion } = this.props
     return (
       <div>
-        TodoList
+        {
+        todos.map((todo) => (<Todo key={todo.id} todo={todo} toggleCompletion={toggleCompletion}/>))
+        }
+      
       </div>
     )
   }
